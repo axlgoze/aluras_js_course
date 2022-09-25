1557ebc4c67f53b6972f571d279f5f2a04e42736
Axel Reyes <rbaxel.f@gmail.com>
2022-09-25 17:15:00 -0500

    Step 02) ADD: cuenta de ahorro de Axel

--- index.js
+++ index.js
@@ -2,39 +2,17 @@
 import {Cliente} from './Cliente.js'
 import {CuentaCorriente} from './CuentaCorriente.js'
 import { CuentaAhorro } from './CuentaAhoro.js';
+
 //Creacion de instancias
-// creacion de cliente axel
+// creacion de clientes
 const cliente = new Cliente('Axel','1234543','1231231');
-
-// creacion de cuenta de axel
-const cuentaDeAxel = new CuentaCorriente(cliente,'1','001');
-
-// console.log(cuentaDeAxel);
-// console.log("****************");
-
-// let saldo = cuentaDeAxel.verSaldo();
-// console.log(`El saldo de la cuenta corriente de ${cliente.nombreCliente} es: ${saldo}`);
-
-// saldo = cuentaDeAxel.deposito(150);
-// console.log(`El saldo de la cuenta corriente de ${cliente.nombreCliente} es: ${saldo}`);
-
-// creacion de cliente de maria
 const cliente2 = new Cliente('María','1236544','1233281');
 
-// creacion de cuenta de maria
+// creacion de cuentas
+const cuentaDeAxel = new CuentaCorriente(cliente,'1','001');
 const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
-// por referencia tiene una instancia de cliente
-// cuentaDeMaria.cliente = cliente2;
-
-// console.log(cuentaDeMaria.cliente)
-
-// cuentaDeAxel.transferirCuenta(100,cuentaDeMaria);
-// console.log("cuenta de Mária: ", cuentaDeMaria)
-
-
-// const saldoMaria= cuentaDeMaria.verSaldo();
-// console.log(`El saldo de la cuenta corriente de María es: ${saldoMaria}`);
-
-// const saldoAxel = cuentaDeAxel.verSaldo();
-// console.log(`El saldo de la cuenta corriente de ${cliente.nombreCliente} es: ${saldoAxel}`);
-console.log(CuentaCorriente.cantidadCuentas)
+// creacion de cuentas de ahorro
+const cuentaDeAhorroAxel = new CuentaAhorro(cliente,'9985','1','001',0);
+console.log(cuentaDeAxel);
+console.log('******')
+console.log(cuentaDeAhorroAxel);
