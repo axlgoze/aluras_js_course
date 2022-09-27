bd0cb7f7597999f2bafe7c572dde0770b44db12f
Axel Reyes <rbaxel.f@gmail.com>
2022-09-27 13:27:59 -0500

    Step 10) ADD: Gestión de Empleados

--- index.js
+++ index.js
@@ -1,47 +1,22 @@
 // importacion de clases
 import {Cliente} from './Cliente.js'
-import { CuentaCorriente } from './CuentaCorriente.js';
-import { CuentaAhorro } from './CuentaAhorro.js';
-import { Cuenta } from './Cuenta.js';
-import { CuentaNomina } from './CuentaNomina.js';
+import { Director } from './Empleados/Director.js';
+// import { CuentaCorriente } from './Cuentas/CuentaCorriente.js';
+// import { CuentaAhorro } from './Cuentas/CuentaAhorro.js';
+// import { Cuenta } from './Cuenta.js';
+// import { CuentaNomina } from './Cuentas/CuentaNomina.js';
+import { Empleado } from './Empleados/Empleado.js';
+import { Gerente } from './Empleados/Gerente.js';
 
 //Creacion de instancias
 // creacion de clientes
 const cliente = new Cliente('Axel','1234543','1231231');
 const cliente2 = new Cliente('María','1236544','1233281');
 
-// creacion de cuentas
-const cuentaDeAxel = new CuentaCorriente(cliente,'1','001');
-const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
-// creacion de cuentas de ahorro
-const cuentaDeAhorroAxel = new CuentaAhorro(cliente,'9985','001',0);
+const empleado = new Empleado ('Juan Perez', '123',10000);
+const gerente = new Gerente('Pedro Rivas', '456', 12000);
+const director = new Director('Elena Moreno','789', 15000);
 
-// console.log(cuentaDeAxel);
-// cuentaDeAxel.deposito(150);
-// console.log(`El saldo de la cuenta de ${cliente.nombreCliente} es:`);
-// console.log(cuentaDeAxel.verSaldo());
-// cuentaDeAxel.retiro(50);
-// console.log(`El saldo de la cuenta de ${cliente.nombreCliente} es:`);
-// console.log(cuentaDeAxel.verSaldo());
-
-// console.log(cuentaDeAhorroAxel)
-// cuentaDeAhorroAxel.deposito(150);
-// console.log(`El saldo de la cuenta de ${cliente.nombreCliente} es:`);
-// console.log(cuentaDeAhorroAxel.verSaldo());
-// cuentaDeAhorroAxel.retiro(50);
-// console.log(`El saldo de la cuenta de ${cliente.nombreCliente} es:`);
-// console.log(cuentaDeAhorroAxel.verSaldo());
-
-console.log(cuentaDeAhorroAxel);
-// ahora no se puede instanciar la clase base
-// const cuentaSimple = new Cuenta(cliente,'098','001',100);
-// console.log(cuentaSimple)
-
-
-const cuentaNominaAxel = new CuentaNomina(cliente,'004','004',100);
-console.log(cuentaNominaAxel);
-cuentaNominaAxel.deposito(150);
-console.log(cuentaNominaAxel.verSaldo());
-
-cuentaNominaAxel.retiro(50);
-console.log(cuentaNominaAxel.verSaldo());
+console.log(empleado.verBonificacion());
+console.log(gerente.verBonificacion());
+console.log(director.verBonificacion());
